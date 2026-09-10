2787513fd14cb0d9205e8395532dc1f4f93a424a
LuanCHR <lujesuseterno6@gmai.com>
2026-09-10 00:21:29 -0300

    Exercício 1 - Classes de apoio

---
+++ main.dart
@@ -0,0 +1,21 @@
+class Passageiro {
+  String? nome;
+  String? cpf;
+  String? rg;
+  String? email;
+  String? celular;
+}
+
+class PlataformaVenda {
+  int? codigoCanal;
+  String? nomeCanal;
+}
+
+class Atendente {
+  String? nome;
+  String? matricula;
+  String? cargo;
+  String? email;
+  String? celular;
+  double? salario;
+}
